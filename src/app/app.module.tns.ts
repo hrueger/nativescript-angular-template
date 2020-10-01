7c90aae25940e03aa7f52355412c67f2455b55d2
Hannes Rüger <hannesrueger@gmx.de>
2020-10-01 20:11:11 +0200

    fixed imports

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -1,8 +1,8 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from '@nativescript/angular';
 
-import { AppRoutingModule } from '@src/app/app-routing.module.tns';
-import { AppComponent } from '@src/app/app.component';
+import { AppRoutingModule } from './app-routing.module.tns';
+import { AppComponent } from './app.component';
 
 
 // Uncomment and add to NgModule imports if you need to use two-way binding and/or HTTP wrapper
